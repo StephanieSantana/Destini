df8fdfd5d9da427fb1b05a12730552d8ff594d24
stephaniesantana <stephycsantana@gmail.com>
2020-05-12 14:32:26 -0700

    completed MVC practice

---
+++ Controller/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var stories = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        stories.nextStory(userInput: sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = stories.getTitle()
+        choice1Button.setTitle(stories.answerAChosen(), for: .normal)
+        choice2Button.setTitle(stories.answerBChosen(), for: .normal)
+    }
+}
+
